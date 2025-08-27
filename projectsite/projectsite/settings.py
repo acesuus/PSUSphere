4e8bdee141eb4541acaa101d244dc20ef01de7c3
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-08-28 00:35:39 +0800

    modified host name

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+kh9e78k@9kygq7*70@j(8h3y1dmjxm#1kmb71%c05&3pg+k!0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['kert.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
