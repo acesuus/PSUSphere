64a6dda4bdae79d47eb4f206254f1a0fa6c865e4
unknown <rudgeremojo21@gmail.com>
2025-08-29 22:49:53 +0800

    modify refactor

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -5,8 +5,7 @@ from .models import College, Program, Organization, Student, OrgMember
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+
 
 
 @admin.register(Student)
