b949d14a9dc1a425b78269bcfdfdc3136742dc1a
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-08-31 18:54:05 +0800

    fix error

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -3,14 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(Program)
-admin.site.register(Organization)
 
 
-@admin.register(College)
-class CollegeAdmin(admin.ModelAdmin):
-    list_display = ("college_name", "created_at", "updated_at",)
-    search_fields = ("college_name",)
 
 
 @admin.register(College)
