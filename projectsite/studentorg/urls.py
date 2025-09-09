0812c0243a4e69a73499c50f402cac7e79c58405
unknown <rudgeremojo21@gmail.com>
2025-09-09 23:15:24 +0800

    modified urls.py

--- projectsite/studentorg/urls.py
+++ projectsite/studentorg/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from studentorg.views import HomePageView, OrganizationList
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('organization_list', OrganizationList.as_view(), name='organization-list'),
 ]
