77f543bcedf9be49d6fd070808be5c9b239191d7
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-09-09 19:43:38 +0800

    modified urls.py

--- projectsite/studentorg/urls.py
+++ projectsite/studentorg/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from studentorg.views import HomePageView
+from studentorg import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+path("admin/", admin.site.urls),
+path('', views.HomePageView.as_view(), name='home'),
 ]
