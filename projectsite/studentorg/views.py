4c31e861f8cae8304a161575cc8296eabfa18131
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-09-09 19:42:15 +0800

    modified views.py

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from studentorg.models import Organization
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
 
 def home(request):
     return HttpResponse("Django install worked successfully!")
