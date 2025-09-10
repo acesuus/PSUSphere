f063553afb52d310e516f1498132d2d1cf8ece83
Elaine Mari Saban Buni <rudgeremojo21@gmail.com>
2025-09-10 21:23:44 +0800

    modified urls.py

--- projectsite/studentorg/urls.py
+++ projectsite/studentorg/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from studentorg.views import HomePageView, OrganizationList
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),  # Use the class-based view
-    path('organization_list/', OrganizationList.as_view(), name='organization-list'),  # Added trailing slash
+    path('organization_list/', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),  # Added trailing slash
 ]
