fa48b0125e6f1f673ad8a74f87b7c75abbfddf53
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-09-10 22:44:41 +0800

    fixed delete btn

--- projectsite/studentorg/urls.py
+++ projectsite/studentorg/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('organization_list/', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),  # Added trailing slash
     path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
-    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization- delete'),
+    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
 ]
