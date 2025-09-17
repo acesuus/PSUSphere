9f8161b4b681cfb713957c298c820f0362666d1f
Elaine Mari Saban Buni <rudgeremojo21@gmail.com>
2025-09-17 18:47:40 +0800

    added get_ordering func @  views.py

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -38,6 +38,7 @@ class OrganizationList(ListView):
     context_object_name = 'organizations'
     template_name = 'org_list.html'
     paginate_by = 5
+    ordering = ["college__college_name","name"]
 
 def get_queryset(self):
     qs = super().get_queryset()
@@ -132,6 +133,14 @@ class ProgramList(ListView):
     context_object_name = 'programs'
     template_name = 'program/program_list.html'
     paginate_by = 5
+
+def get_ordering(self):
+    allowed = ["prog_name", "college__college_name"]
+    sort_by = self.request.GET.get("sort_by")
+    if sort_by in allowed:
+        return sort_by
+    return "prog_name"
+
 
 
 class ProgramCreateView(CreateView):
