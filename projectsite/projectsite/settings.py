01d5e43ca5aa1cb02c5d27bb2e90c904251ef6d8
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-09-24 20:54:23 +0800

    dsds

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -48,9 +48,10 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.github',
 ]
 if "pythonanywhere" in socket.gethostname():
-    SITE_ID = 2 # production site (psusphere.pythonanywhere.com)
+    SITE_ID = 3 # production site (psusphere.pythonanywhere.com)
 else:
-    SITE_ID = 1 # local site (127.0.0.1:8000)
+    SITE_ID = 4 # local site (127.0.0.1:8000)
+
 AUTHENTICATION_BACKENDS = [
 'django.contrib.auth.backends.ModelBackend',
 'allauth.account.auth_backends.AuthenticationBackend',
